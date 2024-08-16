8aea8bd9b20b41a9dc0ee28e8b9e9090c4a09cfb
yurimacena <yurimacenadev@gmail.com>
2024-08-16 08:14:59 -0300

    examples of variables types in Java

---
+++ types-of-variables/src/TypesOfVariables.java
@@ -0,0 +1,16 @@
+public class TypesOfVariables {
+    public static void main(String[] args) throws Exception {
+        //primitive types
+        //study the string class that represents text in the application  
+        
+        double maximumSalary = 2500; //two thousand five hundred
+        double minimumSalary = 2.500; //two point five
+
+        System.out.println("Maximum salary: " + maximumSalary);
+        System.out.println("Minimum salary: " + minimumSalary);
+
+        final double PI_VALUE = 3.14; //final in the beginning of the class indicates that class is immutable and the name in caps lock is a convention
+
+        System.out.println(PI_VALUE);
+    }
+}
